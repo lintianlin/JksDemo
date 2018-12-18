f78e0afc43b0e57e72547fbcb178148b83af1d00
lintianlin <mhjdxlwb@sina.com>
2018-12-18 09:54:37 +0800

    第一次提交

---
+++ src/main/java/com/sinfeeloo/jksdemo/Hello.java
@@ -0,0 +1,14 @@
+package com.sinfeeloo.jksdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public String sayHello() {
+        return "3. Hello jenkins, " + new Date();
+    }
+}
---
+++ src/main/java/com/sinfeeloo/jksdemo/JksdemoApplication.java
@@ -0,0 +1,14 @@
+package com.sinfeeloo.jksdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JksdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JksdemoApplication.class, args);
+    }
+
+}
+
