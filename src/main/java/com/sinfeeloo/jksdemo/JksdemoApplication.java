539a2dec9cc00068b150d61811ec1b8c333213be
lintianlin <mhjdxlwb@sina.com>
2018-12-25 16:35:43 +0800

    测试12

--- src/main/java/com/sinfeeloo/jksdemo/JksdemoApplication.java
+++ src/main/java/com/sinfeeloo/jksdemo/JksdemoApplication.java
@@ -2,13 +2,17 @@ package com.sinfeeloo.jksdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JksdemoApplication {
-
-    public static void main(String[] args) {
+public class JksdemoApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JksdemoApplication.class);
+    }
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(JksdemoApplication.class, args);
     }
-
 }
 
