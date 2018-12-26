e57c477d541f524ffff83e598cbd47458ead617d
lintianlin <mhjdxlwb@sina.com>
2018-12-26 14:24:17 +0800

    测试18

--- src/main/java/com/sinfeeloo/jksdemo/JksdemoApplication.java
+++ src/main/java/com/sinfeeloo/jksdemo/JksdemoApplication.java
@@ -1,10 +1,21 @@
 package com.sinfeeloo.jksdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class JksdemoApplication {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(JksdemoApplication.class, args);
