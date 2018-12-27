f0b444eb9e90ae8ef40d937f33e0ff2035ea3b64
lintianlin <mhjdxlwb@sina.com>
2018-12-27 08:51:52 +0800

    测试4

--- src/main/java/com/sinfeeloo/jksdemo/Application.java
+++ src/main/java/com/sinfeeloo/jksdemo/Application.java
@@ -1,32 +1,21 @@
 package com.sinfeeloo.jksdemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
-@ComponentScan
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 此处Application.class替换为springboot默认启动类
         return builder.sources(Application.class);
     }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
 }
+
 
